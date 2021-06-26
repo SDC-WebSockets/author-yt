97ece41071233642c0710e8edd759d8fe375091a
VirtCoder <joekic@gmail.com>
2021-06-25 23:44:31 -0400

    added crud path to api

--- database/author.js
+++ database/author.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose').set('debug', true);
 mongoose.connect('mongodb://localhost/author', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
 
 var db = mongoose.connection;
@@ -9,52 +9,80 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
 let authorSchema = new mongoose.Schema({
-  'authorId': Number,
-  'firstName': String,
-  'middleName': String,
-  'lastName': String,
-  'job': String,
-  'employer': String,
-  'rating': Number,
-  'reviews': Number,
-  'students': Number,
-  'courses': Number,
-  'thumbnail': String, // will be a URL
-  'bio': String
+    'authorId': Number,
+    'firstName': String,
+    'middleName': String,
+    'lastName': String,
+    'job': String,
+    'employer': String,
+    'rating': Number,
+    'reviews': Number,
+    'students': Number,
+    'courses': Number,
+    'thumbnail': String, // will be a URL
+    'bio': String
 });
 
 let Author = mongoose.model('Author', authorSchema);
 
 let save = (records) => {
-  records.forEach(record => {
-    let entry = new Author({
-      authorId: record.authorId,
-      firstName: record.firstName,
-      middleName: record.middleName,
-      lastName: record.lastName,
-      job: record.job,
-      employer: record.employer,
-      rating: record.rating,
-      reviews: record.reviews,
-      students: record.students,
-      courses: record.courses,
-      thumbnail: record.thumbnail,
-      bio: record.bio,
-      captions: record.captions
+    records.forEach(record => {
+        let entry = new Author({
+            authorId: record.authorId,
+            firstName: record.firstName,
+            middleName: record.middleName,
+            lastName: record.lastName,
+            job: record.job,
+            employer: record.employer,
+            rating: record.rating,
+            reviews: record.reviews,
+            students: record.students,
+            courses: record.courses,
+            thumbnail: record.thumbnail,
+            bio: record.bio,
+            captions: record.captions
+        });
+        Promise.resolve(entry.save())
+            .then(doc => console.log('Saved', doc._doc.authorId))
+            .catch(err => console.log(err));
     });
-    Promise.resolve(entry.save())
-      .then(doc => console.log('Saved', doc._doc.authorId))
-      .catch(err => console.log(err));
-  });
+};
+
+let update = (record, callback) => {
+    console.log('updating ', record.authorId);
+    Author.findOneAndUpdate(
+        { authorId: record.authorId },
+        record, { new: true }
+    )
+        .then((result) => {
+            console.log(result);
+            callback(null);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+};
+
+let deleteRec = (authorId, callback) => {
+    console.log('deleting ', authorId);
+    Author.deleteOne({ authorId: authorId })
+        .then((result) => {
+            console.log(result);
+            callback(null);
+        })
+        .catch(err => {
+            console.log(err);
+            callback(err);
+        });
 };
 
 let get = (authorId, callback) => {
-  Author.find({authorId: authorId})
-    .then(doc => callback(doc))
-    .catch(err => console.log(err));
+    Author.find({ authorId: authorId })
+        .then(doc => callback(doc))
+        .catch(err => console.log(err));
 };
-
-
 
 module.exports.save = save;
 module.exports.get = get;
+module.exports.update = update;
+module.exports.deleteRec = deleteRec;
--- server/server.js
+++ server/server.js
@@ -3,32 +3,97 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 app.use(cors());
 app.use(express.static(__dirname + '/../public'));
 
-app.get('/author?:authorId', function(req, res) {
-  let authorId = req.query.authorId;
-  db.get(authorId, function(rec) {
-    console.log(rec);
-    if (rec[0] === undefined) {
-      res.status(404).send([]);
-    } else {
-      res.send({
-        'authorId': rec[0].authorId,
-        'firstName': rec[0].firstName,
-        'middleName': rec[0].middleName,
-        'lastName': rec[0].lastName,
-        'job': rec[0].job,
-        'employer': rec[0].employer,
-        'rating': rec[0].rating,
-        'reviews': rec[0].reviews,
-        'students': rec[0].students,
-        'courses': rec[0].courses,
-        'thumbnail': rec[0].thumbnail, // will be a URL
-        'bio': rec[0].bio
-      });
-    }
-  });
+app.get('/author?:authorId', function (req, res) {
+    let authorId = req.query.authorId;
+    db.get(authorId, function (rec) {
+        console.log(rec);
+        if (rec[0] === undefined) {
+            res.status(404).send([]);
+        } else {
+            res.send({
+                'authorId': rec[0].authorId,
+                'firstName': rec[0].firstName,
+                'middleName': rec[0].middleName,
+                'lastName': rec[0].lastName,
+                'job': rec[0].job,
+                'employer': rec[0].employer,
+                'rating': rec[0].rating,
+                'reviews': rec[0].reviews,
+                'students': rec[0].students,
+                'courses': rec[0].courses,
+                'thumbnail': rec[0].thumbnail, // will be a URL
+                'bio': rec[0].bio
+            });
+        }
+    });
+});
+
+app.post('/author', function (req, res) {
+    const data = [];
+    let author = req.body;
+    console.log(req.body);
+    let bio = `${author.firstName} ${author.middleName} ${author.lastName} works for ${author.employer}`;
+    data.push({
+        'authorId': author.authorId,
+        'firstName': author.firstName,
+        'middleName': author.middleName,
+        'lastName': author.lastName,
+        'job': author.job,
+        'employer': author.employer,
+        'rating': (Number.parseFloat((Math.random() * 2) + 3).toFixed(1)),
+        'reviews': Math.floor(Math.random() * 100000),
+        'students': Math.floor(Math.random() * 1000000),
+        'courses': Math.floor(Math.random() * 90) + 10,
+        'thumbnail': author.thumbnail,
+        'bio': bio
+    });
+    db.save(data);
+    res.status(200).json({
+        message: req.body
+    });
+});
+
+app.put('/author?:authorId', function (req, res) {
+    let author = req.body;
+    console.log('PUT METHOD =====> ', req.body);
+    let bio = `${author.firstName} ${author.middleName} ${author.lastName} works for ${author.employer}`;
+    db.update({
+        'authorId': author.authorId,
+        'firstName': author.firstName,
+        'middleName': author.middleName,
+        'lastName': author.lastName,
+        'job': author.job,
+        'employer': author.employer,
+        'rating': (Number.parseFloat((Math.random() * 2) + 3).toFixed(1)),
+        'reviews': Math.floor(Math.random() * 100000),
+        'students': Math.floor(Math.random() * 1000000),
+        'courses': Math.floor(Math.random() * 90) + 10,
+        'thumbnail': author.thumbnail,
+        'bio': bio
+    }, (err) => {
+        if (err) {
+            res.status(404).send('failed to update');
+        } else {
+            res.status(200).send('data has been updated');
+        }
+    });
+});
+
+app.delete('/author?:authorId', function (req, res) {
+    let authorId = req.query.authorId;
+    db.deleteRec(authorId, (err) => {
+        if (err) {
+            res.status(404).send('failed to delete');
+        } else {
+            res.status(200).send('data has been deleted');
+        }
+    });
 });
 
 module.exports = app;
