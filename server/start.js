10cd2b3ce14cdda9ad49946d89a04ac9dd5150b3
VirtCoder <joekic@gmail.com>
2021-08-11 22:36:35 -0400

    uncomment the clustering steps

--- server/start.js
+++ server/start.js
@@ -1,23 +1,23 @@
-// const cluster = require('cluster');
-// const numCPUs = require('os').cpus().length;
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
 
 const app = require('./server.js');
 const PORT = process.env.PORT || 4095;
 
-// if (cluster.isMaster) {
-//     console.log(`Master ${process.pid} is running`);
-//     for (let i = 0; i < numCPUs; i++) {
-//         cluster.fork();
-//     }
-//     cluster.on('exit', (worker, code, signal) => {
-//         console.log(`worker ${worker.process.pid} died`);
-//     });
-// }
-// else {
+if (cluster.isMaster) {
+    console.log(`Master ${process.pid} is running`);
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+    cluster.on('exit', (worker, code, signal) => {
+        console.log(`worker ${worker.process.pid} died`);
+    });
+}
+else {
     const serverInstance = app.listen(PORT, (err) => {
-        console.log(`Overview is listening at port http://localhost:${PORT}`);
-        /* err ? console.log("Error in server setup") :
-        console.log(`Server is listening at port http://localhost:${PORT} - worker ${process.pid}`); */
+        // console.log(`Overview is listening at port http://localhost:${PORT}`);
+        err ? console.log("Error in server setup") :
+        console.log(`Server is listening at port http://localhost:${PORT} - worker ${process.pid}`);
     });
     
     const closeServer = () => {
@@ -25,4 +25,4 @@ const PORT = process.env.PORT || 4095;
     }
     
     exports.closeServer = closeServer;
-// }
+}
